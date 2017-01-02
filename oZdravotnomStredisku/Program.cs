f1511c443bc38c0a0e00d874ec2723e7d63d6c47
Gusepe Werdy <gusepe.werdy@gmail.com>
2017-01-02 22:32:16 +0100

    Prve riešenie o Zdravotnom stredisku

---
+++ oZdravotnomStredisku/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace liahen
+{
+    class oZdravotnomStredisku
+    {
+        static void Main(string[] args)
+        {
+            string sLine;
+            int row = 0;
+            int stitok = 0;
+            List<pacient> cakaren = new List<pacient>();
+
+            //int max = 250000;
+            //Random rnd = new Random();
+            //string[] aTest = new string[max];
+            //for (int i = 0; i < (max - 1); i++)
+            //{
+
+            //    if ((i % 2) == 0)
+            //    {
+            //        aTest[i] = "dalsi";
+            //    }
+            //    else
+            //    {
+            //        aTest[i] = "pacient " + (i*1000 + 1) + " " + rnd.Next(10000, 1000000000);
+            //    }
+
+            //}
+            //aTest[max - 1] = "koniec";
+
+            //string[] aTest = {  "dalsi",
+            //                    "pacient 1 10",
+            //                    "pacient 2 2",
+            //                    "pacient 13 5",
+            //                    "dalsi",
+            //                    "pacient 17 0",
+            //                    "pacient 999 100",
+            //                    "dalsi",
+            //                    "pacient 3 5",
+            //                    "dalsi",
+            //                    "dalsi",
+            //                    "dalsi",
+            //                    "dalsi",
+            //                    "dalsi",
+            //                    "koniec"
+            //};
+
+            while ((sLine = Console.ReadLine().Trim().ToLower()) != "koniec")
+            //while ((sLine = aTest[row].Trim().ToLower()) != "koniec")
+            {
+                row++;
+                //Console.WriteLine(sLine);
+                var aLine = sLine.Split(' ', ',');
+                switch (aLine[0].ToLower())
+                {
+                    case "dalsi":
+                        if (cakaren.Count == 0)
+                        {
+                            Console.WriteLine("-1");
+                        }
+                        else
+                        {
+                            Console.WriteLine(cakaren.First().cislo);
+                            cakaren.Remove(cakaren.First());
+                        }
+                        //Console.WriteLine();
+                        break;
+                    case "pacient":
+                        stitok++;
+                        cakaren.Add(new pacient( stitok, Int32.Parse(aLine[1]), Int32.Parse(aLine[2])));
+                        cakaren.Sort();
+                        break;
+                }
+            }
+            //Console.WriteLine(" --- Koniec --- Stlač enter ---");
+            //Console.ReadLine();
+        }
+
+        public class pacient : IComparable
+        {
+            public int poradie;
+            public int cislo;
+            public int priorita;
+
+            public pacient(int aPoradie, int aCislo, int aPriorita)
+            {
+                this.poradie = aPoradie;
+                this.cislo = aCislo;
+                this.priorita = aPriorita;
+            }
+
+            public int CompareTo(object obj)
+            {
+                //(pacient)obj;
+                if (((pacient)obj).priorita == this.priorita)
+                    if (((pacient)obj).poradie > this.poradie)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 1;
+                    };
+                if (((pacient)obj).priorita > this.priorita) return 1;
+                else return -1;
+            }
+        }
+    }
+}
---
+++ oZdravotnomStredisku/readme.md
@@ -0,0 +1,44 @@
+﻿#Zdravotné stredisko
+V Nalomenej Trieske majú zdravotné stredisko.V tomto zimnom období doňho prichádza veľa pacientov.Dennodenne si niekto vytkne členok alebo zlomí nohu. V škole vyhlásili chrípkové prázdniny.Hrozí chrípková epidémia.Aj choroba šialených tiav vyčíňa ako besná...
+
+V zdravotnom stredisku na pohotovosti nepretržite ordinuje jeden doktor.Čakáreň plná a noví pacienti prichádzajú a prichádzajú... A každému treba pomôcť – ale komu skôr? Na tejto pohotovosti to riešia tak, že vždy, keď doktor ošetrí jedného pacienta, sestrička zavolá z čakárne dnu toho, kto vyzerá byť najviac chorý. (Ak je takých pacientov viac, tak toho z nich, ktorý čaká najdlhšie.)
+
+Tvojou úlohou je spraviť simuláciu zdravotného strediska.Na začiatku je čakáreň prázdna.Tvoj program má postupne spracovávať tieto príkazy:
+
+"pacient cislo stav" – do čakárne prišiel nový pacient, jeho evidenčné číslo je cislo a číslo stav udáva, ako chorý tento pacient vyzerá(čím je väčšie, tým viac je pacient chorý). Obe čísla sú nezáporné celé a neprevyšujú 1 000 000 000.
+"dalsi" – do ordinácie má ísť ďalší pacient.
+"koniec" – na dnes končíme, odteraz má službu pohotovosť na opačnom konci mesta, presuňte sa tam.
+##Vstup
+
+Vstup obsahuje niekoľko riadkov, v každom z nich je jeden z vyššie popísaných príkazov. Môžeš predpokladať, že žiadni dvaja pacienti nemajú rovnaké evidenčné číslo. Príkazov bude vo vstupnom súbore najviac 250 000.
+
+Pozor! Niektoré testovacie vstupy sú veľké.Riešenia v C++, ktoré používajú cin a cout, ich možno nestihnú v časovom limite načítať.Používajte namiesto toho funkcie scanf a printf.
+
+##Výstup
+
+Vždy, keď prečítaš zo vstupu príkaz dalsi, vypíš jeden riadok a v ňom evidenčné číslo pacienta, ktorý sa práve dostal do ordinácie.V prípade, že je čakáreň práve prázdna, namiesto čísla pacienta vypíš -1.
+
+##Príklad
+
+###vstup
+dalsi
+pacient 1 10
+pacient 2 2
+pacient 3 5
+dalsi
+pacient 17 0
+pacient 999 100
+dalsi
+pacient 13 5
+dalsi
+koniec
+
+###výstup
+-1
+1
+999
+3
+
+##Vysvetlenie
+
+Keď doktor volá posledného pacienta, sú v čakárni dvaja rovnako chorí pacienti (s číslami 3 a 13). Do ordinácie ide pacient číslo 3, lebo čaká dlhšie.
