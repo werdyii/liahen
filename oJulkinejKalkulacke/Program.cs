6ada2627afb20bd22f815dcb896500aee254db74
werdyii <gusepe.werdy@gmail.com>
2017-02-21 01:41:10 +0100

    cterate BuildParseTree

--- oJulkinejKalkulacke/Program.cs
+++ oJulkinejKalkulacke/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace liahen
 {
@@ -10,29 +7,75 @@ namespace liahen
     {
         static void Main(string[] args)
         {
-            int i;
             string sLine = Console.ReadLine();
-            int n = sLine.Length;
+            while (sLine != "")
+            {
+                TreeArithmatic myArithmatic = new TreeArithmatic();
+                myArithmatic.BuildParseTree(sLine);
+                myArithmatic.compute(myArithmatic.currentTree);
+                Console.WriteLine(myArithmatic.myResult());
+                sLine = Console.ReadLine();
+            }
 
-            Tree myTree = new Tree("+");
-            myTree.left = new Tree("*"); myTree.right = new Tree("*");
-            myTree.left.left = new Tree("12"); myTree.left.right = new Tree("3");
-            myTree.right.left = new Tree("4"); myTree.right.right = new Tree("5");
-            compute(myTree);
-            System.out.println("Result is " + values.pop());//expect 12*3+4*5 = 56
-            System.out.println("Result (improved method) is " + computerWithoutstack(myTree));
-
+            //expect 12*3+4*5 = 56
+            //Tree myTree = new Tree("+");
+            //myTree.left = new Tree("*"); myTree.right = new Tree("*");
+            //myTree.left.left = new Tree("12"); myTree.left.right = new Tree("3");
+            //myTree.right.left = new Tree("4"); myTree.right.right = new Tree("5");
         }
-
 
         class TreeArithmatic
         {
 
-            static Stack<Int32> value = new Stack<int>();
+            static Stack<Int32> value;
+            public Tree currentTree;
+
+            public TreeArithmatic()
+            {
+               value  = new Stack<int>();
+            }
 
             private static Boolean ifOperator(String s)
             {
                 return s.Equals("+") || s.Equals("-") || s.Equals("*") || s.Equals("/");
+            }
+
+            public void BuildParseTree(string AritmmeticExpress)
+            {
+                Stack<Tree> tStack = new Stack<Tree>();
+                currentTree = new Tree("");
+                tStack.Push(currentTree);
+                
+                for (int x = 0; x < AritmmeticExpress.Length; x++)
+                {
+                    char ch = AritmmeticExpress[x];
+
+                    switch (ch)
+                    {
+                        case '(':
+                            currentTree.left = new Tree("");
+                            tStack.Push(currentTree);
+                            currentTree = currentTree.left;
+                            break;
+                        case '+':
+                        case '-':
+                        case '*':
+                        case '/':
+                            currentTree.value = ch.ToString();
+                            currentTree.right = new Tree("");
+                            tStack.Push(currentTree);
+                            currentTree = currentTree.right;
+                            break;
+                        case ')':
+                            currentTree = tStack.Pop();
+                            break;
+                        default:
+                            currentTree.value = ch.ToString();
+                            Tree parent = tStack.Pop();
+                            currentTree = parent;
+                            break;
+                    }
+                }
             }
 
             private static int  compute(char op, int a, int b)
@@ -47,7 +90,7 @@ namespace liahen
                 return -1;
             }
 
-            public static void compute(Tree myTree)
+            public void compute(Tree myTree)
             {
                 if(myTree != null)
                 {
@@ -57,8 +100,22 @@ namespace liahen
                     {
                         value.Push(Int32.Parse(myTree.value));
                     }
+                    else
+                    {
+                        int a = value.Pop();
+                        int b = value.Pop();
+                        char op = Char.Parse(myTree.value);
+                        int tmpResult = compute(op, b, a);
+                        value.Push(tmpResult);
+                    }
                 }
             }
+
+            public string myResult()
+            {
+                //return "Result is " + value.Pop();
+                return  value.Pop().ToString();
+            } 
         }
 
         // definovanie jednoducheho stromu
